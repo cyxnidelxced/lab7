56ab041e7fd216ee572a05aff064ff33f324eabe
cyxnidelxced <jnguyen257@insite.4cd.edu>
2024-11-30 14:54:30 -0800

    button event listener

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 // API URLs
 const factsApiUrl = 'https://uselessfacts.jsph.pl/api/v2/facts/random';
 const triviaApiUrl = 'https://opentdb.com/api.php?amount=10';
+
+// Button to trigger loading of facts and trivia
+document.getElementById('load-button').addEventListener('click', loadData);
+
+// Button to show the answer for trivia questions
+document.getElementById('show-answer-btn').addEventListener('click', showAnswer);
+
+let triviaData = [];
+let currentTriviaIndex = 0;
